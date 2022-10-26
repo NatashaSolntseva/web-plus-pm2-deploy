89a9d11851a796a1e089cb1a7b5a417d20e63cc1
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2022-10-26 16:02:01 +0300

    feat: dotenv add, config add

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,40 @@
+const dotenev = require('dotenv');
+
+dotenev.config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'api-mesto-backend',
+      script: '.src/app.ts',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp .env env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm i && npm run build',
+    },
+  },
+};
+
+/*
+в файле, описывающем деплой бэкенда, добавлены pre-deploy и post-deploy команды.
+pre-deploy копирует
+env -файлы на сервер, а post-deploy устанавливает зависимости,
+выполняет сборку и запускает бэкенд на
+удалённом сервере;
+*/
