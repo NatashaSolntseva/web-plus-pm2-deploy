b4f60e66d74f818690079a178f84813853a03f89
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2022-10-29 00:17:26 +0300

    fix: front config

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd frontend && npm i && npm run build && cp -r build/* /var/www/frontend',
+      'post-deploy': 'cd frontend && npm i && npm run build && cp -r build/* /var/www/html',
     },
   },
 };
